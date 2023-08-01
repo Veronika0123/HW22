003ceee523769831ee901915cc095b1ab5bfd9bc
Veronika0123 <veronikamokrii@gmail.com>
2023-08-01 16:10:08 +0300

    /

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,17 @@
 
     let math = Math.floor(Math.random() * 100) + 1;
+    console.log(` ${math} `);
     i=0;
+    
+
+    
 
     function game(){
 
+        let button1 = document.getElementById('elem');
+
         let num = +numInput.value;
+        console.log(` ${num} `);
         
         t.innerHTML = 10 - i;
 
@@ -14,10 +21,18 @@
                 b = 'а загаданое число больше';
             else if (num > math)
                 b = 'а загаданое число меньше';
-            else
-                b = 'вы правы поздравляю';
-
+            else {
+                b = 'вы правы, поздравляю!!!';
+                button1.disabled = true;
+                button1.style = "background-color: gray;";
+            }
+        }
+        else  {
+            button1.disabled = true;
+            m.innerHTML = 'Правельный ответ: ' + math;
+            button1.style = "background-color: gray;";
         }
 
+        
         g.innerHTML = num + ' ' + b;
     }
